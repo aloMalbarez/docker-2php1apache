f978400362e50b856af21f862136238547cf8802
aloMalbarez <aloMalbarez@users.noreply.github.com>
2018-07-22 17:32:40 -0300

    Go

---
+++ code5/index.php
@@ -0,0 +1,22 @@
+<?php
+echo "<pre>";
+echo "V5 ".__DIR__.PHP_EOL;
+$DB_HOST = "db";
+$DB_NAME = getenv('MYSQL_DATABASE');
+$DB_USER = getenv('MYSQL_USER');
+$DB_PASS = getenv('MYSQL_PASSWORD');
+
+// PDO
+try {
+  $conn = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASS);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "[PDO] DB successfully connected";
+}
+catch(PDOException $e) {
+  echo "[PDO] DB connection failed: " . $e->getMessage();
+}
+echo "</pre>";
+?><br/>
+<img src="img/checkmark-2.png" />
+<?php echo "Some info:".PHP_EOL; ?><br/>
+<?php phpinfo(); ?>
